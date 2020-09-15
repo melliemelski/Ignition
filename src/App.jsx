015e8bf6c6d9901b00be8bacba9f5c7757d5540e
mel <melissa.sargent@hotmail.co.uk>
2020-09-15 09:23:10 +0100

    Component folders setup and Jumbotron ready to code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component }from 'react';
+import './App.css';
+import Jumbotron from './components/Jumbotron';
+
+class App extends Component {
+  render () {
+  return (
+    <>
+     <p>app works</p>
+     <Jumbotron></Jumbotron>
+    </>  
+  );
+};
+}
+export default App;
---
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import styles from "./Jumbotron.module.scss";
+
+
+class Jumbotron extends Component {
+  render() { 
+  return (
+    <p>Jumbotron</p>
+  )
+  }
+}
+
+export default Jumbotron;
